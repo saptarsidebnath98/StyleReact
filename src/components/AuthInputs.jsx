76426d8b00be1bba24b26164e541c3bc7ceb8da9
saptarsidebnath98 <saptarsidebnath666666@gmail.com>
2024-06-15 21:11:02 +0530

    deploying to Github

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react';
 import {styled} from 'styled-components';
+import Button from './Button';
+import Input from './Input';
 
-const controlDiv = styled.div`
+const ControlDiv = styled.div`
 display: flex;
 flex-direction: column;
 gap: 0.5rem;
 margin-bottom: 1.5rem;
 ` 
-
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
   const [enteredPassword, setEnteredPassword] = useState('');
@@ -30,31 +31,32 @@ export default function AuthInputs() {
 
   return (
     <div id="auth-inputs">
-      <controlDiv>
-        <p>
-          <label>Email</label>
-          <input
+      <ControlDiv>
+        
+         
+          <Input
+            label="email"
+            invalid={emailNotValid}
             type="email"
             className={emailNotValid ? 'invalid' : undefined}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
-        </p>
-        <p>
-          <label>Password</label>
-          <input
+          <Input
+            label="password"
+            invalid={passwordNotValid}
             type="password"
             className={passwordNotValid ? 'invalid' : undefined}
             onChange={(event) =>
               handleInputChange('password', event.target.value)
             }
           />
-        </p>
-      </controlDiv>
+
+      </ControlDiv>
       <div className="actions">
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
+        <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
